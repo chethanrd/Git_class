6b27143ee2cb96885fb7a0ff033c39b8998d8762
chethanrd <dchethan786@gmail.com>
2026-08-25 12:01:29 +0530

    25-08

---
+++ List_class_P/my_mode.py
@@ -0,0 +1,27 @@
+#modules in python
+
+import math
+import numpy as np
+
+def my_add(a,b):
+    return a+b
+
+lst=[1,2,3,4,5,6,7,8,9]
+
+def is_even(num):
+    return True if num%2==0 else False
+
+def my_filter(func,seq):
+    out_lst=[]
+    for ele in seq:
+        if func(ele)==True:
+            out_lst.append(ele)
+    return out_lst
+
+print(my_filter(is_even,lst)
+
+#def main():
+ #   my_filter(is_even,lst)
+
+# when we execute the module directly, then __main__ is set to __main__
+# when we import the module directly, then __name__ is set to __module_name__
